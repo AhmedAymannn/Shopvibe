bc3700c3fedd265435edd450ab5b429efd84df7c
ahmed ayman <https://github.com/AhmedAymannn>
2025-03-02 02:28:40 +0200

    added the order service , routes , controller , updating product modelcart

---
+++ routes/order.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const orderController = require('../controllers/oreder');
+
+router.use(authMiddleware.protectWithHeaders)
+router.route('/')
+    .get(orderController.getMyOrders)
+    .post(orderController.createOrder)
+router.route('/:id')
+    .get(orderController.getOrder)
+    .patch(orderController.updateMyOrder)
+    .delete(orderController.cancelMyOrder)
